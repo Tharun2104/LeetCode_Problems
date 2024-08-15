b75a361637f58466f5a5e6a4a8b9e2dec0114681
Tharun Teja Mogili <tharunmogili09@gmail.com>
2024-08-15 15:58:22 -0400

    Permutations

---
+++ Recursion/Problem_77.py
@@ -0,0 +1,28 @@
+"""
+https://leetcode.com/problems/combinations/description/
+"""
+
+
+from typing import List
+
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+
+        def dfs(i, cur):
+            if (len(cur) == k):
+                res.append(cur.copy())
+                return
+            if(i == n+1):
+                return
+
+            cur.append(i)
+            dfs(i+1,cur)
+            cur.pop()
+            dfs(i+1,cur)
+
+        dfs(1, [])
+        return res
+            
+        
